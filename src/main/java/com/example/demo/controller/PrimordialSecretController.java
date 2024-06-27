0b5b00098bbb42040270745fad91aa08149059d5
Kileana27 <blond.camillelm@gmail.com>
2024-06-27 11:29:04 +0200

    Added services

--- src/main/java/com/example/demo/controller/PrimordialSecretController.java
+++ src/main/java/com/example/demo/controller/PrimordialSecretController.java
@@ -1,10 +1,35 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.PrimordialSecret;
+import com.example.demo.service.PrimordialSecretService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/primordial-secrets")
 public class PrimordialSecretController {
+
+    @Autowired
+    private PrimordialSecretService primordialSecretService;
+
+    @Operation(summary = "Get all anecdotic secrets", description = "Get all anecdotic secrets")
+    @GetMapping("")
+    public List<PrimordialSecret> getAll() {
+        return this.primordialSecretService.getAll();
+    }
+
+    @Operation(summary = "Get item by id", description = "Get item by id")
+    @GetMapping("/{id}")
+    public PrimordialSecret getById(@PathVariable Long id) {
+        return this.primordialSecretService.getById(id);
+    }
 
 }
---
+++ src/main/java/com/example/demo/service/AnecdoticSecretService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.AnecdoticSecret;
+import com.example.demo.repository.AnecdoticSecretRepository;
+
+@Service
+public class AnecdoticSecretService {
+
+    @Autowired
+    private AnecdoticSecretRepository anecdoticSecretRepository;
+
+    public List<AnecdoticSecret> getAll() {
+        List<AnecdoticSecret> anecdoticSecrets = this.anecdoticSecretRepository.findAll();
+        return anecdoticSecrets;
+    }
+
+    public AnecdoticSecret getById(Long id) {
+        AnecdoticSecret anecdoticSecret = this.anecdoticSecretRepository.findById(id).get();
+        return anecdoticSecret;
+    }
+
+}
