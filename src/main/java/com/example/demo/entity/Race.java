636f33ee268a81c438aea49f5ae27759ab130c0e
Vivi Valienne <valiendanse@gmail.com>
2024-07-02 15:40:24 +0200

    Add Race Entity and Race Repository

---
+++ src/main/java/com/example/demo/entity/Race.java
@@ -0,0 +1,113 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "race")
+public class Race {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private String racialTrait;
+  private Integer minAge;
+  private Integer maxAge;
+  private Integer minHeight;
+  private Integer maxHeight;
+  private Integer speed;
+
+  public Race() {
+  }
+
+  public Race(Long id, String name, String description, String racialTrait, Integer minAge, Integer maxAge,
+      Integer minHeight, Integer maxHeight, Integer speed) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.racialTrait = racialTrait;
+    this.minAge = minAge;
+    this.maxAge = maxAge;
+    this.minHeight = minHeight;
+    this.maxHeight = maxHeight;
+    this.speed = speed;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getRacialTrait() {
+    return racialTrait;
+  }
+
+  public void setRacialTrait(String racialTrait) {
+    this.racialTrait = racialTrait;
+  }
+
+  public Integer getMinAge() {
+    return minAge;
+  }
+
+  public void setMinAge(Integer minAge) {
+    this.minAge = minAge;
+  }
+
+  public Integer getMaxAge() {
+    return maxAge;
+  }
+
+  public void setMaxAge(Integer maxAge) {
+    this.maxAge = maxAge;
+  }
+
+  public Integer getMinHeight() {
+    return minHeight;
+  }
+
+  public void setMinHeight(Integer minHeight) {
+    this.minHeight = minHeight;
+  }
+
+  public Integer getMaxHeight() {
+    return maxHeight;
+  }
+
+  public void setMaxHeight(Integer maxHeight) {
+    this.maxHeight = maxHeight;
+  }
+
+  public Integer getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(Integer speed) {
+    this.speed = speed;
+  }
+
+}
--- src/main/java/com/example/demo/repository/RaceRepository.java
+++ src/main/java/com/example/demo/repository/RaceRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.Race;
+
 @Repository
 public interface RaceRepository extends JpaRepository<Race, Long> {
 
