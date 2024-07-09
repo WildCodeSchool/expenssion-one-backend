f4471a28fe00742b46d7c7b57c2a863b13527c0c
Sebastien Peltier <sebpel62@gmail.com>
2024-07-09 12:11:05 +0200

    correction

--- src/main/java/com/example/demo/repository/CharacterRepository.java
+++ src/main/java/com/example/demo/repository/CharacterRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Character;
+import com.example.demo.entity.User;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character,Long>{
