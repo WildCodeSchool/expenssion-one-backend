6b60a587e651d3ce5bd8c65aab3dd4d947c782c3
Kileana27 <blond.camillelm@gmail.com>
2024-07-10 15:14:25 +0200

    Changed

--- src/main/java/com/example/demo/ExpenssionOne.java
+++ src/main/java/com/example/demo/ExpenssionOne.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.service.DbService;
+
 @SpringBootApplication
 public class ExpenssionOne {
 
 	public static void main(String[] args) {
 		var app = new SpringApplication(ExpenssionOne.class);
 		var ctx = app.run(args);
-		// DbService dbService = ctx.getBean(DbService.class);
-		// dbService.insertDB();
+		DbService dbService = ctx.getBean(DbService.class);
+		dbService.insertDB();
 	}
 
 }
